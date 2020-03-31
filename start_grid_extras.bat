20aebc5c52097884ac03cf7ba33c50b4c1b05cb9
Jeevan Adiga <Jeevan-adiga@users.noreply.github.com>
2020-04-01 00:09:37 +0530

    Update start_grid_extras.bat

--- start_grid_extras.bat
+++ start_grid_extras.bat
@@ -1,1 +1,1 @@
-java -jar "D:\SeleniumGridExtras\SeleniumGridExtras-2.0.4-SNAPSHOT-jar-with-dependencies.jar"
+java -jar "C:\Users\administrator\SeleniumGridExtras\SeleniumGridExtras-2.0.4-SNAPSHOT-jar-with-dependencies.jar"
